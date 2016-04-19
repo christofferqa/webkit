7265ed2ee6e253ae4bd894f45243aab9e04ab004
Christoffer Quist Adamsen <c.adamsen@partner.samsung.com>
2016-04-19 10:40:00 -0700

    Options

--- auto_explore_site.sh
+++ auto_explore_site.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
+
 SITE=$1
+shift
+
+ROBOT_TIMEOUT="15"
+ROBOT_EXTRA_TIME="5"
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        --h)
+            echo "Usage: ./auto_explore_site.sh [--robot-timeout=<num>] [--robot-extra-time=<num>]"
+            exit 0
+            ;;
+        --robot-timeout=*)
+            ROBOT_TIMEOUT="${1#*=}"
+            ;;
+        --robot-extra-time=*)
+            ROBOT_EXTRA_TIME="${1#*=}"
+            ;;
+        *)
+            echo "Error: Invalid argument: $1."
+            exit 1
+    esac
+    shift
+done
+
 echo $SITE > /tmp/url_file
 WebKitBuild/Release/bin/QtTestBrowser -r /tmp/url_file \
-       -robot-timeout 15 -robot-extra-time 5 -maximize -auto-explore
+       -robot-timeout $ROBOT_TIMEOUT -robot-extra-time $ROBOT_EXTRA_TIME -maximize -auto-explore
 ls -l ER_actionlog
